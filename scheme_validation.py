a9c42ca4554eaafb93ca91038cb00e92a164432d
Michał Kadlof <mkadlof@pzh.gov.pl>
2024-10-22 15:02:35 +0200

    refactored scheme_validation.py

--- scheme_validation.py
+++ scheme_validation.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
 import json
+
 import fastjsonschema
-#import os
-#os.chdir('/home/michall/Desktop/testy_tmp/json_schema/podtest')
-# dwa argumenty pierwszy to oczekiwany schemat jsona drugi to json ktory chcemy sprawdzic czy do schematu pasuje
-schemat = json.load(open(sys.argv[1]))
-validate = fastjsonschema.compile(schemat)
 
-analizowany_json = json.load(open(sys.argv[2]))
 
-print(validate(analizowany_json))
+def validate_json(schema_file: str, json_file_path: str):
+    with open(schema_file) as schema_file:
+        schema_file = json.load(schema_file)
+        validate = fastjsonschema.compile(schema_file)
+    with open(json_file_path) as json_file:
+        json_file = json.load(json_file)
+    return validate(json_file)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Validate JSON file against JSON schema')
+    parser.add_argument('schema', type=str, help='JSON schema file')
+    parser.add_argument('json', type=str, help='JSON file to validate')
+    args = parser.parse_args()
+
+    validation_result = validate_json(args.schema, args.json)
+    print(validation_result)
+
+
+if __name__ == '__main__':
+    main()
